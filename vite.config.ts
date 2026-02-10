f7e2f94ca6f78d7637db3db6509aac3d1ebe2287
liuji <ji.liu@transwarp.io>
2026-02-10 19:59:57 +0800

    chore: 修复打包配置并重新构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineConfig({
   plugins: [react()],
+});
+
+const vitestConfig = defineVitestConfig({
   test: {
     globals: true,
     environment: "jsdom",
@@ -17,3 +21,5 @@ export default defineConfig({
     },
   },
 });
+
+export default mergeConfig(viteConfig, vitestConfig);
